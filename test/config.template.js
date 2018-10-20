9257ee5e37545824634ecc3dddb8baa1b142d07a
thomaslule <thomas@lule.fr>
2018-10-20 14:18:20 +0200

    more simple test

--- test/config.template.js
+++ test/config.template.js
@@ -1,19 +1,10 @@
 module.exports = {
-  bot: {
-    channel: '',
-    username: '',
-    token: '',
-    client_id: '',
-    client_secret: '',
-    callback_url: 'http://your_ip:8080/',
-    port: 8080,
-    secret: '',
-  },
-  streamer: {
-    channel: '',
-    username: '',
-    token: '',
-    activate_polling: false,
-    activate_webhook: false,
-  },
+  channel: '',
+  username: '',
+  token: '',
+  client_id: '',
+  client_secret: '',
+  callback_url: 'http://your_ip:8080/',
+  port: 8080,
+  secret: '',
 };
--- test/index.js
+++ test/index.js
@@ -1,16 +1,12 @@
 const Channel = require('..');
 const options = require('./config');
 
-const botChannel = Channel(options.bot);
-const streamerChannel = Channel(options.streamer);
+const channel = Channel(options);
 
-botChannel.on('chat', (...args) => { console.log('chat', args); });
-botChannel.on('stream-begin', (...args) => { console.log('stream-begin', args); });
-botChannel.on('stream-change-game', (...args) => { console.log('stream-change-game', args); });
-botChannel.on('stream-end', (...args) => { console.log('stream-end', args); });
-botChannel.on('top-clipper-change', (...args) => { console.log('top-clipper-change', args); });
-botChannel.on('follow', (...args) => { console.log('follow', args); });
-botChannel.connect();
-
-streamerChannel.on('host', (...args) => { console.log('host', args); });
-streamerChannel.connect();
+channel.on('chat', (...args) => { console.log('chat', args); });
+channel.on('stream-begin', (...args) => { console.log('stream-begin', args); });
+channel.on('stream-change-game', (...args) => { console.log('stream-change-game', args); });
+channel.on('stream-end', (...args) => { console.log('stream-end', args); });
+channel.on('top-clipper-change', (...args) => { console.log('top-clipper-change', args); });
+channel.on('follow', (...args) => { console.log('follow', args); });
+channel.connect();
