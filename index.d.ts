c6f4e4decd53175f8bc77bee2b45a90b1686e579
thomaslule <thomas@lule.fr>
2018-10-21 14:28:34 +0200

    add typing

---
+++ index.d.ts
@@ -0,0 +1,29 @@
+export = TwitchChannel;
+
+declare function TwitchChannel(options: TwitchChannel.TwitchChannelOptions): TwitchChannel.TwitchChannelInstance;
+
+declare namespace TwitchChannel {
+
+  export interface TwitchChannelInstance {
+    on(event: string, handler: (...args: any[]) => void): void;
+    connect(): Promise<void>;
+    disconnect(): Promise<void>;
+    say(message: string): void;
+    getTopClipper(): Promise<any>;
+  }
+
+  export interface TwitchChannelOptions {
+    channel: string;
+    bot_name: string;
+    bot_token: string;
+    client_id: string;
+    client_secret: string;
+    streamlabs_socket_token: string;
+    callback_url: string;
+    secret: string;
+    port?: number;
+    logger: any;
+    is_test?: boolean;
+  }
+
+}
