88d47b0cd469fa50f04316f57d8bcdf323d0a400
thomaslule <thomas@lule.fr>
2018-11-14 10:30:43 +0100

    more safeguards around streamlabs events

--- src/streamlabs.ts
+++ src/streamlabs.ts
@@ -3,29 +3,34 @@ import { Options } from "./options";
 import { TwitchChannel } from "./twitch-channel";
 
 export class Streamlabs {
-  private socket: SocketIOClient.Socket | undefined;
+  private socket: SocketIOClient.Socket;
+  private handledMessages = new Set<string>();
 
   constructor(private twitchChannel: TwitchChannel, private options: Options) {
+    this.socket = io(`https://sockets.streamlabs.com?token=${this.options.streamlabs_socket_token}`, {
+      autoConnect: false,
+    });
+    this.socket.on("event", (event: any) => this.eventHandler(event));
   }
 
   public start() {
-    this.stop();
-    this.socket = io(`https://sockets.streamlabs.com?token=${this.options.streamlabs_socket_token}`);
-    this.socket.on("event", (event: any) => this.eventHandler(event));
+    this.socket.open();
     this.twitchChannel.emit("info", "listening to streamlabs events");
   }
 
   public stop() {
-    if (this.socket !== undefined) {
-      this.socket.close();
-    }
+    this.socket.close();
   }
 
   private async eventHandler(event: any) {
     try {
-      if (!event.message) { return; }
+      this.twitchChannel.emit("debug", `streamlabs event: ${JSON.stringify(event)}`);
+      if (!Array.isArray(event.message)) { return; }
       await Promise.all(event.message.map(async (streamlabsMsg: any) => {
         if (!this.options.is_test && streamlabsMsg.isTest) {
+          return;
+        }
+        if (this.isDuplicateMessage(streamlabsMsg)) {
           return;
         }
         if (event.type === "donation") {
@@ -54,5 +59,17 @@ export class Streamlabs {
     } catch (err) {
       this.twitchChannel.emit("error", err);
     }
+  }
+
+  // Streamlabs messages are often sent twice. To avoid that, remember them for 1mn
+  private isDuplicateMessage(message: any) {
+    if (message._id) {
+      if (this.handledMessages.has(message._id)) {
+        return true;
+      }
+      this.handledMessages.add(message._id);
+      setTimeout(() => { this.handledMessages.delete(message._id); }, 60000);
+    }
+    return false;
   }
 }
