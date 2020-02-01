e29e5f790f8906ff8a469ef955236f9629aad012
thomaslule <thomas@lule.fr>
2020-02-01 18:13:14 +0100

    Stronger typing for on() method

--- src/twitch-channel.ts
+++ src/twitch-channel.ts
@@ -26,6 +26,107 @@ export class TwitchChannel extends EventEmitter {
     });
   }
 
+  public on(event: "debug", handler: (param: string) => void): this;
+  public on(event: "info", handler: (param: string) => void): this;
+  public on(event: "error", handler: (param: string | object) => void): this;
+  public on(
+    event: "chat",
+    handler: (param: {
+      viewerId: string;
+      viewerName: string;
+      message: string;
+    }) => void
+  ): this;
+  public on(
+    event: "cheer",
+    handler: (param: {
+      viewerId: string;
+      viewerName: string;
+      amount: number;
+      message: string;
+    }) => void
+  ): this;
+  public on(
+    event: "sub",
+    handler: (param: {
+      viewerId: string;
+      viewerName: string;
+      message?: string;
+      plan?: "Prime" | "1000" | "2000" | "3000";
+      planName?: string;
+    }) => void
+  ): this;
+  public on(
+    event: "resub",
+    handler: (param: {
+      viewerId: string;
+      viewerName: string;
+      message?: string;
+      months?: number;
+      plan?: "Prime" | "1000" | "2000" | "3000";
+      planName?: string;
+    }) => void
+  ): this;
+  public on(
+    event: "subgift",
+    handler: (param: {
+      viewerId: string;
+      viewerName: string;
+      recipientId: string;
+      recipientName: string;
+      plan?: "Prime" | "1000" | "2000" | "3000";
+      planName?: string;
+    }) => void
+  ): this;
+  public on(
+    event: "host",
+    handler: (param: {
+      viewerId: string;
+      viewerName: string;
+      viewers: number;
+    }) => void
+  ): this;
+  public on(
+    event: "raid",
+    handler: (param: {
+      viewerId: string;
+      viewerName: string;
+      viewers: number;
+    }) => void
+  ): this;
+  public on(
+    event: "follow",
+    handler: (param: { viewerId: string; viewerName: string }) => void
+  ): this;
+  public on(
+    event: "ban",
+    handler: (param: { viewerId: string; viewerName: string }) => void
+  ): this;
+  public on(
+    event: "stream-begin",
+    handler: (param: { game: string }) => void
+  ): this;
+  public on(
+    event: "stream-change-game",
+    handler: (param: { game: string }) => void
+  ): this;
+  public on(event: "stream-end", handler: () => void): this;
+  public on(
+    event: "streamlabs/donation",
+    handler: (param: {
+      viewerId?: string;
+      viewerName: string;
+      amount: number;
+      currency: string;
+      message?: string;
+    }) => void
+  ): this;
+  public on(event: string | symbol, handler: (...args: any[]) => void): this;
+  public on(event: string | symbol, handler: (...args: any[]) => void): this {
+    super.on(event, handler);
+    return this;
+  }
+
   public async connect() {
     if (this.options.streamlabs_socket_token) {
       this.streamlabs!.start();
